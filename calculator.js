3348b207f8bbd1c5c837fc1f05cc11ce0c37f48d
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-13 00:11:25 -0500

    created app.post for post request and correct route in index.html

--- calculator.js
+++ calculator.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/', (req, res) => {
+  res.send('Thanks for posting that.');
+});
+
 app.listen(3000, () => {
   console.log('Sending from port 3000.');
 });
