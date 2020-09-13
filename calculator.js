3df56ed7569ab35e302e2efddb3416929df285a2
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-13 01:18:57 -0500

    added express parser to pass the http request and obtain access to the date

--- calculator.js
+++ calculator.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', (req, res) => {
-  res.send('Thanks for posting that.');
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+
+  res.send('The result of this calculation is: ' + result);
 });
 
 app.listen(3000, () => {
